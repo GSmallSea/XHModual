ef05abf0b044192514a1eb04ba70e4dcfccd0797
GSmallSea <gxiao_hai@163.com>
2018-11-16 10:15:18 +0800

    first commit

---
+++ XHModual/CHCXHModual.m
@@ -0,0 +1,13 @@
+//
+//  CHCXHModual.m
+//  CHC_Manager
+//
+//  Created by XH on 2018/11/16.
+//  Copyright © 2018年 XH. All rights reserved.
+//
+
+#import "CHCXHModual.h"
+
+@implementation CHCXHModual
+
+@end
